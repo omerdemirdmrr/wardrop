ab63a22249d043174af83447542c607400dd32e3
YavuzAvcii <yavperen@gmail.com>
2025-12-29 15:04:58 +0300

    fixed again

--- backend/api/services/emailService.js
+++ backend/api/services/emailService.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
   service: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
